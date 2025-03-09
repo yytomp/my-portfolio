addb120d8811d81ca8bfa3edcf45b71c9ed12a58
Thomas Peng <thomas.peng@lshauto.com.au>
2025-03-10 10:32:21 +1100

    Update Footer & dyanmic Contact content

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(cors()); // for front-end access
 
 // Serve static files from the "public" directory
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 
